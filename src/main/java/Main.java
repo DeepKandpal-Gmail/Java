e6fecd5de23024046db63d683ed19a3687f27a3a
MOHD_SHAMSUL_HAQ <MOHD_SHAMSUL_HAQ>
2017-01-21 20:35:33 +0530

    :):)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,11 +33,16 @@ public class Main {
       Connection connection = null;
       Map<String, Object> attributes = new HashMap<>();
       try {
-		
-		String dbUrl = System.getenv("USERS_DB_URL");
-        connection = DriverManager.getConnection(dbUrl);
+		  
+		  private static Connection getConnection() throws URISyntaxException, SQLException {
+    URI dbUri = new URI(System.getenv("USERS_DB_URL"));
 
-        Statement stmt = connection.createStatement();
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+    connection= DriverManager.getConnection(dbUrl, username, password);
+		Statement stmt = connection.createStatement();
         stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
         stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
         ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
