573c2960ea5466a16dcacc53fcf5c52023e0a85f
deepkandpal16@gmail.com <deepkandpal16@gmail.com>
2017-01-23 16:44:42 +0530

    connecting new data base ----

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello Deep");
+    get("/hello", (req, res) -> "Hello Deep Kandpal");
 
     get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
